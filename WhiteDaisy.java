6148dafa6458025ed9ff3e2c4b5d55a3ceda4645
markwp02 <markwp02@gmail.com>
2015-09-05 12:02:58 +1000

    Create WhiteDaisy.java
    Has a higher birthrate in warmer temperatures and albedo reflects most light and cools Daisyworld.

---
+++ WhiteDaisy.java
@@ -0,0 +1,25 @@
+
+public class WhiteDaisy extends Daisy{
+
+	
+	public WhiteDaisy(double localTemp, int i, int j) {
+		super("images\\white.jpg", localTemp, i, j);
+		this.albedo = 0.75;//reflect 75% of solar radiation
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

63c608991fcb790f46cb559a0d11080b020721fe
markwp02 <markwp02@gmail.com>
2015-09-05 12:07:25 +1000

    Create Daisy.java
    Abstract class that is a parent to the different daisies.

---
+++ Daisy.java
@@ -0,0 +1,16 @@
+
+public abstract class Daisy extends Thing{
+
+	public Daisy(String image, double LocalTemp, int i, int j){
+		super(image, LocalTemp,i,j);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

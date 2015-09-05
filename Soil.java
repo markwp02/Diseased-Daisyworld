29797bb787620a7036708dce696fafdcac8fd36c
markwp02 <markwp02@gmail.com>
2015-09-05 12:08:52 +1000

    Create Soil.java
    Abstract soil class that is a parent to both types of soil.

---
+++ Soil.java
@@ -0,0 +1,19 @@
+
+public abstract class Soil extends Thing{
+
+	public Soil(String image, double localTemp,int i, int j) {
+		super(image, localTemp,i,j);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}

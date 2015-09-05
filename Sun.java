f2bdcb52aa60088ae49bcad036174aca579f47f1
markwp02 <markwp02@gmail.com>
2015-09-05 11:58:03 +1000

    Create Sun.java
    Sun class increases solar radiation or luminosity over time

---
+++ Sun.java
@@ -0,0 +1,26 @@
+
+public class Sun {
+
+	double luminosity;
+	final double increase = 0.001;
+	public Sun(){
+		luminosity = 1.0;
+	}
+	
+	public void updateSun(){
+		luminosity += increase;
+	}
+	
+	public double getLuminosity(){
+		return luminosity;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

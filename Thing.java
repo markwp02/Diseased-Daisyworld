4ed4c3a7ecc237b85cda2e913126467bdf546f34
markwp02 <markwp02@gmail.com>
2015-09-05 12:06:31 +1000

    Create Thing.java
    Abstract class, that is a parent class to everything in the grid.

---
+++ Thing.java
@@ -0,0 +1,37 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ * 
+ */
+
+/**
+ * @author Mark
+ *
+ */
+public abstract class Thing {
+
+	JButton jb;
+	double localTemp;
+	int i,j;
+	double albedo;
+	
+	public Thing(String image, double localTemp,int i,int j){
+		jb = new JButton(new ImageIcon(image));
+		this.localTemp = localTemp;
+		this.i=i;
+		this.j=j;
+	}
+	
+	abstract void update();
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
